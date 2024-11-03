22e974594f54b932c3af5fd972015cc17d848cba
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2024-11-03 23:44:24 +0300

    Auth Set Up

--- Spotify/Controllers/Other/AuthViewController.swift
+++ Spotify/Controllers/Other/AuthViewController.swift
@@ -6,24 +6,74 @@
 //
 
 import UIKit
+import WebKit
 
-class AuthViewController: UIViewController {
-
+class AuthViewController: UIViewController, WKNavigationDelegate {
+    
+    private let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs
+        let webView = WKWebView(frame: .zero, configuration: config)
+        return webView
+    }()
+    
+    public var completionHandler: ((Bool) -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Sign In"
+        view.backgroundColor = .systemBackground
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+        
+        guard let url = AuthManager.shared.signInURL else {
+            return
+        }
+        webView.load(URLRequest(url: url))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
     }
-    */
-
+    
+    //    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    //        guard let url = webView.url else {
+    //            return
+    //        }
+    //        print("2")
+    ////        Exchange the code for access token
+    //        guard let code = URLComponents(string: url.absoluteString)?.queryItems?.first(where: {
+    //
+    //            $0.name == "code"
+    //
+    //        })?.value else {
+    //            print("5")
+    //            return
+    //        }
+    //
+    //        print("Code")
+    //        print(code)
+    //    }
+   
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        if let url = navigationAction.request.url {
+            print("Navigating to URL: \(url.absoluteString)")
+            
+            // Kod parametresini içeren URL kontrolü
+            if url.absoluteString.starts(with: "spotify-ios-quick-start://spotify-login-callback") {
+                if let code = URLComponents(string: url.absoluteString)?.queryItems?.first(where: { $0.name == "code" })?.value {
+                    print("Code: \(code)")
+                    // Kodla ilgili işlemleri burada gerçekleştirin
+                } else {
+                    print("Kod parametresi yok.")
+                }
+            }
+        }
+        
+        decisionHandler(.allow) // Navigasyona izin ver
+    }
 }
--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "a2325ef43f8b4d648a0fc22d688e0f8a"
+        static let clientSecret = "4aab3af7e0464b15b6009f650112160f"
+    }
+
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "spotify-ios-quick-start://spotify-login-callback"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +43,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool? {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping((Bool) -> Void)) {
+//     Get Token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
